9ec62ac3b3bdfaf7f22221e4170021e9a16d623f
Jarvis <jarvis@jarviss-mac.local>
2018-10-15 20:46:22 +0530

    Initial Commit

---
+++ Gif_Image_Loader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gif_Image_Loader
+//
+//  Created by Jarvis on 15/10/18.
+//  Copyright © 2018 Jarvis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
